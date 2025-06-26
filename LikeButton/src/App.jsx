c12f72459a3239a050096a9d312fccca416069b8
Rohit Kumar <rohitrbs2001@gmail.com>
2025-06-26 17:13:24 +0530

    first commit

---
+++ LikeButton/src/App.jsx
@@ -0,0 +1,15 @@
+import LikeButton from './LikeButton'
+import './App.css'
+
+function App() {
+
+   
+  return (
+    <>
+      <p >State in React</p>
+      <LikeButton/>
+    </>
+  )
+}
+
+export default App
---
+++ LikeButton/src/LikeButton.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+export default function LikeButton(){
+
+    let [isliked, setIsliked] = useState(false);
+
+    let toggleLike = () =>{
+        setIsliked(!isliked);
+    }
+    return(
+
+        <div>
+            <p onClick={toggleLike}>
+                {isliked ? (<i className="fa-solid fa-heart" style={{color: "red"}}></i>) : (<i className="fa-regular fa-heart"></i>)}
+            </p>
+        </div>
+    )
+}
